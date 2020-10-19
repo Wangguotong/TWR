d64c77a41a2c6594d8125b192a26ae83614598fb
LIXUANXI\李宣西、 <wangguotong@foxmail.com>
2020-10-19 21:31:21 +0800

    Added title and labels

--- main.m
+++ main.m
@@ -1,10 +1,14 @@
 Xank = 3:1:40;
 Yfank= 400./Xank;
 figure(1)
-plot(Xank, Yfreq);
+plot(Xank, Yfank);
+title('Anchor numbers Vs one tag');
+xlabel('Anchor number');ylabel('one tag freq/Hz')
 
 Xtag = 3:1:40;
 Yftag= 1000./(2.5*Xtag);
 Yftagmin = 1000./(2.5*Xtag + 1);
 figure(2)
 plot(Xtag,Yftag,Xtag,Yftagmin);
+title('Tag numbers Vs tag freq');
+xlabel('tag number');ylabel('tag freq/Hz')
