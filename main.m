4b81f80756a1de76d6751cc6f8aa8aa624411aa3
LIXUANXI\李宣西、 <wangguotong@foxmail.com>
2020-10-19 15:02:45 +0800

    first one

---
+++ main.m
@@ -0,0 +1,10 @@
+Xank = 3:1:40;
+Yfank= 400./Xank;
+figure(1)
+plot(Xank, Yfreq);
+
+Xtag = 1:1:40;
+Yftag= 1000./(2.5*Xtag);
+Yftagmin = 1000./(2.5*Xtag + 1);
+figure(2)
+plot(Xtag,Yftag,Xtag,Yftagmin);
